ed92bbf220633126a89611fb732215e1ec285296
Galu <2411500010@mahasiswa.atmaluhur.ac.id>
2026-04-17 18:06:54 +0700

    pert4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'LoginPage.dart';
+import 'DashboardPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,54 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment:
-                CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20),
-              Text(
-                "Login",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-
-              SizedBox(
-                height: 200,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Username",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 16),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Password",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 24),
-
-              // Tombol Login
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    print("Login ditekan");
-                  },
-                  child: Text("Login"),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/dashboards': (context) => DashboardPage(),
+      },
     );
   }
 }
